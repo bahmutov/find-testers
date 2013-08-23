b27e0c65c3f5bc016b11ae432cd4fb78f17428bd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-23 07:32:49 -0400

    starting loading tests

---
+++ src/test/load-testers.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var load = require('../load-testers');
+
+gt.module('load testers');
+
+var filename = path.join(__dirname, 'testers.csv');
+
+gt.test('load test data', function () {
+  gt.arity(load, 1, 'load takes single argument');
+  var testers = load(filename);
+  // gt.object(testers, 'loaded testers object');
+});
